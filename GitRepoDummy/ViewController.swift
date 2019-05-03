c8ffbc542ebc81940eea5c80fc1a67f372e59296
IOSuser2 <iosuser2@iosuser2s-mac-mini.local>
2019-05-03 10:23:12 +0530

    added yellow colour

--- GitRepoDummy/ViewController.swift
+++ GitRepoDummy/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
+        self.view.backgroundColor = .yellow
         // Do any additional setup after loading the view, typically from a nib.
     }
 
